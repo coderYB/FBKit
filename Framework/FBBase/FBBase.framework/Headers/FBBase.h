d7dd752cdef28686ea73ff01fadef24c0f52e1ed
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-08 09:09:35 +0800

    FB

---
+++ Framework/FBBase/FBBase.framework/Headers/FBBase.h
@@ -0,0 +1,20 @@
+//
+//  FBBase.h
+//  FBBase
+//
+//  Created by 王磊 on 2020/4/8.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBBase.
+FOUNDATION_EXPORT double FBBaseVersionNumber;
+
+//! Project version string for FBBase.
+FOUNDATION_EXPORT const unsigned char FBBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBBase/PublicHeader.h>
+
+
+#import <FBBase/FBBaseViewController.h>
