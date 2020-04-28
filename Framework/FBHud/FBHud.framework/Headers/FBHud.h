15590de945155824b911e39b42bfa904002c6280
StoneStoneStoneWang <yuanxingfu1314@163.com>
2020-04-28 23:24:28 +0800

    FB

---
+++ Framework/FBHud/FBHud.framework/Headers/FBHud.h
@@ -0,0 +1,20 @@
+//
+//  FBHud.h
+//  FBHud
+//
+//  Created by 王磊 on 2020/4/28.
+//  Copyright © 2020 王磊. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBHud.
+FOUNDATION_EXPORT double FBHudVersionNumber;
+
+//! Project version string for FBHud.
+FOUNDATION_EXPORT const unsigned char FBHudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBHud/PublicHeader.h>
+
+
+#import <FBHud/FBHudManager.h>
